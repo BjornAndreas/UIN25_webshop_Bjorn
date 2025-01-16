cf143b93c3b98fcee3c33b3051a7185890fa448b
Bjørn Andreas <70635155+BjornAndreas@users.noreply.github.com>
2025-01-16 10:17:07 +0100

    update

--- Legodudes/shopscript.js
+++ Legodudes/shopscript.js
@@ -10,9 +10,69 @@ products.map((product, index) => productHTML += `<article class="product-card">
                 <a href="#KATEGORISIDE">Ninjago</a>
                 <h3>${product.title}</h3>
                 <p>Kr. ${product.price},-</p>
-                <button>Legg i Handlekurv</button>
+                <button onclick="addProductToCart(${product.prodid})">Legg i Handlekurv</button>
             </article>`)
         
 
 //finn #productlist, og fyll den med verdiene i variabelen productHTML
 document.getElementById("productlist").innerHTML = productHTML
+
+//Lage toggle,funksjonalitet for handlevogn
+document.getElementById("shoppingcart").addEventListener("click", function() {
+    document.getElementById("cart").classList.toggle("visible")
+})
+
+//Lage addProductToCart()
+function addProductToCart(prodid) {
+    console.log("Du vil legge ti produktid " + prodid)
+    //bruk .some for å sjeke om prodid allerede finnes i cart:
+    const idExists = cart.some(cartprod => cartprod.cartprodid === prodid)
+
+    if(idExists) {
+        //oppdatere dette produktets quantity
+        //først: finne indexen til denne ID-en:
+        const index = cart.findIndex(p => p.cartprodid === prodid)
+        //Så: oppdatere riktig quantity
+        cart[index].quantity++
+    } else {
+        cart.push({cartprodid: prodid, quantity: 1})
+    }
+
+    printCart()
+    console.log(cart)
+}
+
+//Lage en funksjon som skriver ut oppdatert versjon av handlevognen
+function printCart() {
+    //Starte med en tom variabel vi kan fylle med HTML
+    let = cartHTML = ""
+    //Lag klar variabel for pris:
+    let cartTotal = 0;
+    //Lag variabel for antall produkter:
+    let cartNumber = 0;
+
+    //Gå gjennom cart-arrayen og generere HTML for hvert produkt:
+    cart.map((cartprod, index)=> {
+        const currentProduct = products.findIndex(p => p.prodid === cartprod.cartprodid)
+        const currentProductInfo = products[currentProduct]
+        cartHTML += `<article class="cart-product">
+                    <span class="title">${currentProductInfo.title}</span>
+                    <span class="price">${currentProductInfo.price},-</span>
+                    <span class="quantity">x<span class="quantity-number">${cartprod.quantity}</span></span>
+                    <button class="delete">x</button>
+                </article>`
+            //Regn ut totalsum:
+            cartTotal += currentProductInfo.price * cartprod.quantity
+            //Regn ut antall produkter:
+            cartNumber += cartprod.quantity
+    })
+
+    //Skrive ut generert HTML til index-fila
+    document.getElementById("cart-products").innerHTML = cartHTML
+    //Skrive ut totalpris:
+    document.getElementById("cart-total").innerHTML = cartTotal
+    //Skrive ut antall produkter:
+    document.getElementById("cartcount").innerHTML = cartNumber
+}
+
+printCart()
